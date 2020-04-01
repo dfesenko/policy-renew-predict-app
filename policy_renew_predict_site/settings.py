52438ae937ac2d729425b6c4faeb4c0e838d33da
dfesenko5 <fesenkogithub@gmail.com>
2020-04-01 22:04:26 +0300

    Prepare for docker deployment

--- policy_renew_predict_site/settings.py
+++ policy_renew_predict_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hq*m!47yf4&^ks4_9%grsdg5$&h_umahxqs_jasd&6rb@+kbi5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
